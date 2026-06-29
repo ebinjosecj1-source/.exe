7a37290732106e6c2ec68a2a57ba771a2d0a339d
ebinjosecj1-source <ebinjosecj1@gmail.com>
2026-06-29 13:51:11 +0530

    Update DeviceEnumerationService.cs

--- src/WindowsScreenRecorder/Services/DeviceEnumerationService.cs
+++ src/WindowsScreenRecorder/Services/DeviceEnumerationService.cs
@@ -10,13 +10,22 @@ namespace WindowsScreenRecorder.Services;
 public class DeviceEnumerationService : IDeviceEnumerationService
 {
     [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate lpfnEnum, IntPtr dwData);
-    private delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref NativeRect lprcMonitor, IntPtr dwData);
+    private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate cb, IntPtr dwData);
+    private delegate bool MonitorEnumDelegate(IntPtr hMon, IntPtr hdc, ref NativeRect rect, IntPtr data);
     [DllImport("user32.dll", CharSet = CharSet.Auto)]
     private static extern bool GetMonitorInfoW(IntPtr hMonitor, ref MONITORINFOEX lpmi);
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-    private struct MONITORINFOEX { public int cbSize; public NativeRect rcMonitor; public NativeRect rcWork; public int dwFlags; [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] public string szDevice; }
+    private struct MONITORINFOEX
+    {
+        public int cbSize;
+        public NativeRect rcMonitor;
+        public NativeRect rcWork;
+        public int dwFlags;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string szDevice;
+    }
+
     [StructLayout(LayoutKind.Sequential)]
     private struct NativeRect { public int left, top, right, bottom; }
 
@@ -24,43 +33,41 @@ public class DeviceEnumerationService : IDeviceEnumerationService
     {
         var monitors = new List<MonitorInfo>();
         int idx = 0;
-        MonitorEnumDelegate del = (IntPtr hMon, IntPtr hdc, ref NativeRect rect, IntPtr data) =>
+        bool Callback(IntPtr hMon, IntPtr hdc, ref NativeRect rect, IntPtr data)
         {
             var mi = new MONITORINFOEX();
             mi.cbSize = Marshal.SizeOf(mi);
             if (GetMonitorInfoW(hMon, ref mi))
             {
-                var bounds = new Rect(mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top);
                 monitors.Add(new MonitorInfo
                 {
                     Index = idx++,
                     DeviceName = mi.szDevice,
                     FriendlyName = mi.szDevice,
-                    Bounds = bounds,
+                    Bounds = new Rect(mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right - mi.rcMonitor.left, mi.rcMonitor.bottom - mi.rcMonitor.top),
                     WorkArea = new Rect(mi.rcWork.left, mi.rcWork.top, mi.rcWork.right - mi.rcWork.left, mi.rcWork.bottom - mi.rcWork.top),
-                    IsPrimary = (mi.dwFlags & 1) != 0,
-                    Handle = hMon
+                    IsPrimary = (mi.dwFlags & 1) != 0
                 });
             }
             return true;
-        };
-        EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, del, IntPtr.Zero);
+        }
+        EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, Callback, IntPtr.Zero);
         return monitors;
     }
 
     public IReadOnlyList<WindowInfo> GetCapturableWindows() => new List<WindowInfo>();
 
     public IReadOnlyList<AudioDeviceInfo> GetAudioOutputDevices() =>
-        new List<AudioDeviceInfo> { new AudioDeviceInfo { Id = "default", Name = "Default Output", IsLoopback = true } };
+        new List<AudioDeviceInfo> { new AudioDeviceInfo { Id = "default", FriendlyName = "Default Output", IsCapture = false } };
 
     public IReadOnlyList<AudioDeviceInfo> GetAudioInputDevices() =>
-        new List<AudioDeviceInfo> { new AudioDeviceInfo { Id = "mic", Name = "Default Microphone", IsLoopback = false } };
+        new List<AudioDeviceInfo> { new AudioDeviceInfo { Id = "mic", FriendlyName = "Default Microphone", IsCapture = true } };
 
     public IReadOnlyList<WebcamInfo> GetWebcams() => new List<WebcamInfo>();
 
     public AudioDeviceInfo? GetDefaultOutputDevice() =>
-        new AudioDeviceInfo { Id = "default", Name = "Default Output", IsLoopback = true };
+        new AudioDeviceInfo { Id = "default", FriendlyName = "Default Output", IsCapture = false };
 
     public AudioDeviceInfo? GetDefaultInputDevice() =>
-        new AudioDeviceInfo { Id = "mic", Name = "Default Microphone", IsLoopback = false };
+        new AudioDeviceInfo { Id = "mic", FriendlyName = "Default Microphone", IsCapture = true };
 }
